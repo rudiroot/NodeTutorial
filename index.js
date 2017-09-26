35c758fcfd247cfcc70f7d7f2c9ed31ea1680b20
Rudolf Haak <info@rudolfhaak.de>
2017-09-26 15:36:46 +0200

    Added Todo Comment

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ app.get('/', function(reg,res) {
     res.send('Hello World');
 }); 
 
+// TO DO GET APPLES
+
 app.listen(3000, function() {
     console.log(`y server is running on port ${port}`);
 });
